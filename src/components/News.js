b5c7a1f8033065b3a0858a6f28906ca6c97772be
Ravindra Kawale <ravindra.kawale@waiin.com>
2024-05-27 18:26:24 +0530

    video 28 completed.

--- src/components/News.js
+++ src/components/News.js
@@ -2,11 +2,70 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            articles: [],
+            loading: false,
+            page: 1
+        }
+    }
+
+    async componentDidMount() {
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=66bd6be758cb406eab30a52aca74a70b&page=1&pageSize=20";
+        let data = await fetch(url).then((response) => {
+            response.json().then((news) => {
+                this.setState({ articles: news.articles, totalResults: news.totalResults })
+            })
+        })
+    }
+
+    handleNextClick = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=66bd6be758cb406eab30a52aca74a70b&page=${this.state.page + 1}&pageSize=20`;
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+        }
+        else {
+            let data = await fetch(url).then((response) => {
+                response.json().then((news) => {
+                    this.setState({ articles: news.articles })
+                })
+            })
+
+            this.setState({ page: this.state.page + 1 });
+        }
+    }
+
+    handlePrevClick = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=66bd6be758cb406eab30a52aca74a70b&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url).then((response) => {
+            response.json().then((news) => {
+                this.setState({ articles: news.articles })
+            })
+        })
+
+        this.setState({ page: this.state.page - 1 });
+    }
+
+
     render() {
         return (
-            <div>
-                This is a news component
-                <NewsItem />
+            <div className='container my-3'>
+                <h2>NewsMonkey - Top Headlines</h2>
+
+                <div className="row">
+                    {this.state.articles.map((element) => {
+                        return <div className="col-md-4" key={element.url}>
+                            <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
+                        </div>
+                    })}
+                </div>
+
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr;Previous</button>
+                    <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                </div>
             </div>
         )
     }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
+        let { title, description, imageUrl, newsUrl } = this.props;
         return (
-            <div>
-
+            <div className='my-3'>
+                <div className="card" style={{ width: "18rem" }}>
+                    <img src={!imageUrl ? 'https://static.toiimg.com/thumb/msid-110436182,width-1070,height-580,imgsize-525399,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg' : imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}...</p>
+                        <a href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
             </div>
         )
     }
